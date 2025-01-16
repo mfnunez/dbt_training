44103e52748a4b54ba67d74bf12c9c9ec9c930f6
mnunez@avisia.fr <mnunez@avisia.fr>
2025-01-16 11:33:33 +0000

    group by customer

--- models/marts/finance/fct_orders.sql
+++ models/marts/finance/fct_orders.sql
@@ -12,6 +12,7 @@ final as(
     select orders.order_id, orders.customer_id, SUM(amount) as livetime_value
     from orders
         left join payments using (order_id)
+    group by 2
 )
 
 select * from final
